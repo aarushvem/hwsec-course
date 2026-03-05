cc46b7b3677fedff778edf6b552a5997c3fccf38
Kavin Shanmugam Sankar <kssankar@burnsville.cs.unc.edu>
2026-03-05 15:51:40 -0500

    sender/receiver implemented

--- lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -1,29 +1,208 @@
-
-#include"util.h"
-// mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
+#include "util.h"
 #include <sys/mman.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#define BUFF_SIZE (1<<21)
+#define L2_WAYS 16
+#ifndef STRIDE
+#define STRIDE (1<<17)
+#endif
+
+#define SET_SPACING 32
+#define BASE_SET 64
+
+static inline uint64_t rdtscp(void) {
+    uint32_t lo, hi;
+    asm volatile("rdtscp" : "=a"(lo), "=d"(hi) :: "rcx");
+    return ((uint64_t)hi << 32) | lo;
+}
+
+struct node {
+    struct node *next;
+    char pad[64 - sizeof(struct node *)];
+};
+
+void *buf;
+struct node *sets[256];
+uint64_t thresholds[256];
+
+void shuffle(struct node **array, int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        struct node *temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+void build_set(int logical_set_index) {
+    char *base = (char *)buf;
+    struct node *nodes[L2_WAYS];
+
+    int physical_set_index = BASE_SET + (logical_set_index * SET_SPACING);
+
+    for (int i = 0; i < L2_WAYS; i++) {
+        nodes[i] = (struct node *)(base + physical_set_index * 64 + i * STRIDE);
+    }
+
+    shuffle(nodes, L2_WAYS);
+
+    for (int i = 0; i < L2_WAYS - 1; i++) {
+        nodes[i]->next = nodes[i+1];
+    }
+    nodes[L2_WAYS-1]->next = NULL;
+
+    sets[logical_set_index] = nodes[0];
+}
+
+void prime_set(int set_index) {
+    struct node *curr = sets[set_index];
+    while (curr) curr = curr->next;
+}
+
+uint64_t probe_set(int set_index) {
+    uint64_t start = rdtscp();
+    struct node *curr = sets[set_index];
+    while (curr) curr = curr->next;
+    uint64_t end = rdtscp();
+    return end - start;
+}
+
+void calibrate(uint64_t manual_threshold) {
+
+    for (int i = 0; i <= 8; i++) {
+
+        if (manual_threshold > 0) {
+            thresholds[i] = manual_threshold;
+        }
+
+        else {
+
+            uint64_t sum = 0;
+            int samples = 1000;
+
+            for (int k = 0; k < samples; k++) {
+                prime_set(i);
+                sum += probe_set(i);
+            }
+
+            uint64_t avg_hit = sum / samples;
+
+            thresholds[i] = avg_hit * 1.5;
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-	// Put your covert channel setup code here
+    srand(time(NULL));
 
-	printf("Please press enter.\n");
+    uint64_t manual_threshold = 0;
+    if (argc > 1) {
+        manual_threshold = strtoull(argv[1], NULL, 10);
+    }
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
+    buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE,
+               MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB,
+               -1, 0);
 
-	printf("Receiver now listening.\n");
+    if (buf == (void*) - 1) {
+        perror("mmap() error\n");
+        exit(EXIT_FAILURE);
+    }
 
-	bool listening = true;
-	while (listening) {
+    *((char *)buf) = 1;
 
-		// Put your covert channel code here
+    for (int i = 0; i <= 8; i++) {
+        build_set(i);
+    }
 
-	}
+    calibrate(manual_threshold);
 
-	printf("Receiver finished.\n");
+    printf("Please press enter.\n");
 
-	return 0;
+    char text_buf[2];
+    fgets(text_buf, sizeof(text_buf), stdin);
+
+    printf("Receiver now listening.\n");
+
+    while (1) {
+
+        prime_set(8);
+
+        for(volatile int k=0; k<20000; k++);
+
+        uint64_t t8 = probe_set(8);
+
+        if (t8 > thresholds[8]) {
+
+            int bit_counts[8] = {0};
+            int samples = 100;
+            int valid_samples = 0;
+
+            for (int s = 0; s < samples; s++) {
+
+                for (int i = 0; i <= 8; i++)
+                    prime_set(i);
+
+                for(volatile int k=0; k<50000; k++);
+
+                uint64_t t_valid = probe_set(8);
+
+                if (t_valid > thresholds[8]) {
+
+                    valid_samples++;
+
+                    for (int i = 0; i < 8; i++) {
+                        if (probe_set(i) > thresholds[i]) {
+                            bit_counts[i]++;
+                        }
+                    }
+                }
+            }
+
+            if (valid_samples > (samples * 0.5)) {
+
+                int received_byte = 0;
+
+                for (int i = 0; i < 8; i++) {
+
+                    if (bit_counts[i] > (valid_samples * 0.75)) {
+                        received_byte |= (1 << i);
+                    }
+                }
+
+                printf("%d\n", received_byte);
+
+                int consecutive_lows = 0;
+                int timeout = 0;
+
+                while(1) {
+
+                    prime_set(8);
+
+                    for(volatile int k=0; k<50000; k++);
+
+                    if (probe_set(8) < thresholds[8])
+                        consecutive_lows++;
+                    else
+                        consecutive_lows = 0;
+
+                    if (consecutive_lows >= 10)
+                        break;
+
+                    timeout++;
+
+                    if (timeout > 2000000)
+                        break;
+                }
+            }
+        }
+    }
+
+    return 0;
 }
-
-
--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -1,45 +1,112 @@
-
-#include"util.h"
-// mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
+#include "util.h"
 #include <sys/mman.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
-// TODO: define your own buffer size
 #define BUFF_SIZE (1<<21)
-//#define BUFF_SIZE [TODO]
+#define L2_WAYS 16
+#ifndef STRIDE
+#define STRIDE (1<<17)
+#endif
+
+#define SET_SPACING 32
+#define BASE_SET 64
+
+static inline uint64_t rdtscp(void) {
+    uint32_t lo, hi;
+    asm volatile("rdtscp" : "=a"(lo), "=d"(hi) :: "rcx");
+    return ((uint64_t)hi << 32) | lo;
+}
+
+struct node {
+    struct node *next;
+    char pad[64 - sizeof(struct node *)];
+};
+
+void *buf;
+struct node *sets[256];
+
+void shuffle(struct node **array, int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        struct node *temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+void build_set(int logical_set_index) {
+    char *base = (char *)buf;
+    struct node *nodes[L2_WAYS];
+
+    int physical_set_index = BASE_SET + (logical_set_index * SET_SPACING);
+
+    for (int i = 0; i < L2_WAYS; i++) {
+        nodes[i] = (struct node *)(base + physical_set_index * 64 + i * STRIDE);
+    }
+
+    shuffle(nodes, L2_WAYS);
+
+    for (int i = 0; i < L2_WAYS - 1; i++) {
+        nodes[i]->next = nodes[i+1];
+    }
+    nodes[L2_WAYS-1]->next = NULL;
+
+    sets[logical_set_index] = nodes[0];
+}
+
+void evict_set(int set_index) {
+    struct node *curr = sets[set_index];
+    while (curr) curr = curr->next;
+    curr = sets[set_index];
+    while (curr) curr = curr->next;
+}
 
 int main(int argc, char **argv)
 {
-  // Allocate a buffer using huge page
-  // See the handout for details about hugepage management
-  void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
-  
+  srand(time(NULL));
+
+  buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE,
+             MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB,
+             -1, 0);
+
   if (buf == (void*) - 1) {
      perror("mmap() error\n");
      exit(EXIT_FAILURE);
   }
-  // The first access to a page triggers overhead associated with
-  // page allocation, TLB insertion, etc.
-  // Thus, we use a dummy write here to trigger page allocation
-  // so later access will not suffer from such overhead.
-  //*((char *)buf) = 1; // dummy write to trigger page allocation
 
+  *((char *)buf) = 1;
 
-  // TODO:
-  // Put your covert channel setup code here
-
-  printf("Please type a message.\n");
+  for (int i = 0; i <= 8; i++) {
+      build_set(i);
+  }
 
   bool sending = true;
   while (sending) {
+      printf("Please type a message.\n");
+	  
       char text_buf[128];
-      fgets(text_buf, sizeof(text_buf), stdin);
+      if (fgets(text_buf, sizeof(text_buf), stdin) == NULL) break;
 
-      // TODO:
-      // Put your covert channel code here
+      int message = atoi(text_buf);
+
+      long duration = 4000000;
+
+      for (long k = 0; k < duration; k++) {
+
+          evict_set(8);
+
+          for (int i = 0; i < 8; i++) {
+              if ((message >> i) & 1) {
+                  evict_set(i);
+              }
+          }
+      }
   }
 
   printf("Sender finished.\n");
   return 0;
 }
-
-
